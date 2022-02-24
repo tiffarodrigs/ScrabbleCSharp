a2a306d62b90df8d4f1ddd2d357534e627b4d51a
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-02-24 13:13:40 -0800

    Update file with score Calculating logic
    Co-authored-by: Jase Seeley <jcseeley31590@yahoo.com>

--- Scrabble/Models/ScoreCalculator.cs
+++ Scrabble/Models/ScoreCalculator.cs
@@ -20,51 +20,29 @@ namespace Scrabble.Models
     //method to calculate the score
     public int calculateScore(string UserWord)
     {
-      Dictionary<char, int> CharacterValues = new Dictionary<char, int>() {
-        {'a', 1},
-        {'e', 1},
-        {'i', 1},
-        {'o', 1},
-        {'u', 1},
-        {'l', 1},
-        {'n', 1},
-        {'r', 1},
-        {'s', 1},
-        {'t', 1},
-        {'d', 2},
-        {'g', 2},
-        {'b', 3},
-        {'c', 3},
-        {'m', 3},
-        {'p', 3},
-        {'f', 4},
-        {'h', 4},
-        {'v', 4},
-        {'w', 4},
-        {'y', 4},
-        {'k', 5},
-        {'j', 8},
-        {'x', 8},
-        {'q', 10},
-        {'z', 10}
+      Dictionary<string, int> CharacterValues = new Dictionary<string, int>() {
+        {"a,e,i,o,u,l,n,r,s,t",1},
+        {"d,g",2},
+        {"b,c,m,p",3},
+        {"f,h,v,w,y",4},
+        {"k",5},
+        {"j,x",8},
+        {"q,z",10}
       };
-      
       char[] wordArr = UserWord.ToCharArray();
       int result = 0;
       for(int i = 0; i < wordArr.Length; i++)
       {
-        result += CharacterValues[wordArr[i]];
-        //.Value;
-      }
-// for(int i=0; i<wordArr.length ;i++)//c
-// {
-//  result += CharacterValues[wordArr[i]]//CharacterValues[c]==>3
-  
-// }
-//iterate the user input through for
-//find the value of the letter inside the for
-
-    return result;
+        string inputCharString= wordArr[i].ToString();
+        foreach (KeyValuePair<string, int> entry in CharacterValues)
+        {
+          if((entry.Key).Contains(inputCharString))
+            {
+              result += entry.Value;
+            }
+        }
+      }  
+      return result;
     }
   }
 }
